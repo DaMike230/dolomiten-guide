a5910251be382fbe7cb13282f9cb15388a4ce917
DaMike230 <mad-dog02@gmx.de>
2026-06-19 11:10:52 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
+void main() {
+  runApp(const DolomitenGuideApp());
+}
+
+class DolomitenGuideApp extends StatelessWidget {
+  const DolomitenGuideApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dolomiten Guide',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.teal,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
